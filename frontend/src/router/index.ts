bc45bcfe71c63defcc086588403e03c7f8b897b6
phelsajc <phelsajc@users.noreply.github.com>
2026-01-22 12:03:34 +0800

    Create main.css and fix router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+        path: '/',
+        redirect: '/login', // redirects root to /login
+    },
+    {
       path: '/login',
       name: 'login',
       component: LoginView
