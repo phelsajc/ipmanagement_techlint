0cef052c7bc6d750a310fd2e3a517dbd5f249c77
phelsajc <phelsajc@users.noreply.github.com>
2026-01-22 14:14:37 +0800

    Add navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <div class="app-layout">
+    <Header />
     <RouterView v-slot="{ Component }">
       <transition name="fade" mode="out-in">
         <component :is="Component" />
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="app-header card">
+    <nav class="nav-container">
+      <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+      <router-link to="/audit-logs" class="nav-link">Audit Logs</router-link>
+      <button class="btn btn-primary btn-logout" @click="logout">Logout</button>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const logout = () => {
+    
+}
+</script>
+
+<style scoped>
+.app-header {
+  padding: 1rem 2rem;
+  background-color: #166881;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-container {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.nav-link {
+  color: #fff;
+  font-weight: 500;
+  text-decoration: none;
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+}
+
+.btn-logout {
+  background-color: #e84118;
+  color: white;
+  padding: 0.4rem 0.8rem;
+  border-radius: 4px;
+}
+
+.btn-logout:hover {
+  background-color: #c23616;
+}
+</style>
